0054f075aaea362dc82577e22414c45309a206d8
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2026-02-20 12:26:54 +0530

    added the sitmap blogs

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+// Load blogs from JSON
+function getAdditionalPaths() {
+  const blogsPath = path.join(__dirname, 'data', 'blogs.json');
+  const blogs = JSON.parse(fs.readFileSync(blogsPath, 'utf8'));
+
+  // Add blog index page
+  const paths = [
+    {
+      loc: '/blog',
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date().toISOString(),
+    },
+  ];
+
+  // Add individual blog posts
+  blogs.forEach((blog) => {
+    paths.push({
+      loc: `/blog/${blog.slug}`,
+      changefreq: 'monthly',
+      priority: 0.7,
+      lastmod: blog.date,
+    });
+  });
+
+  return paths;
+}
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://buildbeyondstudio.com',
@@ -5,4 +36,7 @@ module.exports = {
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 0.7,
-}
+  additionalPaths: async (config) => {
+    return getAdditionalPaths();
+  },
+};
